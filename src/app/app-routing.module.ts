2ebb30a0238b06a071d588a6cfee023e00217145
janamava <aizhana.kamalova@gmail.com>
2019-05-14 17:05:48 +0200

    recent progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChessboardComponent } from './chessboard/chessboard.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'chessboard', component: ChessboardComponent },
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'task-manager', component: TaskManagerComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
+import { ChessboardComponent } from './chessboard/chessboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    TaskManagerComponent,
+    ChessboardComponent
   ],
   imports: [
     BrowserModule,
