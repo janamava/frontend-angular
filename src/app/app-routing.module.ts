7e0eddcb8a4178e679a4c245a7093f579a949ed4
janamava <aizhana.kamalova@gmail.com>
2019-05-15 15:25:31 +0200

    added styling, animations, fixed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { ChessboardComponent } from './chessboard/chessboard.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TaskManagerComponent } from './task-manager/task-manager.component';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomeTasksComponent } from './task-manager/home-tasks/home-tasks.component';
+import { ExistingComponent } from './task-manager/existing/existing.component';
+import { FinishedComponent } from './task-manager/finished/finished.component';
 
 const routes: Routes = [
-  {path: 'chessboard', component: ChessboardComponent },
-  {path: 'portfolio', component: PortfolioComponent},
-  {path: 'task-manager', component: TaskManagerComponent }, 
-  {path: 'home', component: AppComponent},
-  {path: 'home',redirectTo: '/home', pathMatch: 'full'}
+  { path: 'chessboard', component: ChessboardComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  {
+    path: 'task-manager', component: TaskManagerComponent, children: [
+      { path: 'home-tasks', component: HomeTasksComponent },
+      { path: 'existing', component: ExistingComponent },
+      { path: 'finished', component: FinishedComponent }
+    ]
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'home', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TaskManagerComponent } from './task-manager/task-manager.component';
 import { ChessboardComponent } from './chessboard/chessboard.component';
+import { HomeComponent } from './home/home.component';
+import { HomeTasksComponent } from './task-manager/home-tasks/home-tasks.component';
+import { ExistingComponent } from './task-manager/existing/existing.component';
+import { FinishedComponent } from './task-manager/finished/finished.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
     TaskManagerComponent,
-    ChessboardComponent
+    ChessboardComponent,
+    HomeComponent,
+    HomeTasksComponent,
+    ExistingComponent,
+    FinishedComponent
   ],
   imports: [
     BrowserModule,
