452e06f7fb3ad6d062fadd829f0faaf7fd671316
janamava <aizhana.kamalova@gmail.com>
2019-05-15 09:07:31 +0200

    added styles to portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ChessboardComponent } from './chessboard/chessboard.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TaskManagerComponent } from './task-manager/task-manager.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path: 'chessboard', component: ChessboardComponent },
   {path: 'portfolio', component: PortfolioComponent},
-  {path: 'task-manager', component: TaskManagerComponent }
+  {path: 'task-manager', component: TaskManagerComponent }, 
+  {path: 'home', component: AppComponent},
+  {path: 'home',redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
