c89cde7c2516453bde759d3b2653a7b47cd2c9b2
janamava <aizhana.kamalova@gmail.com>
2019-05-23 14:41:50 +0200

    Shows the accomplished tasks

--- src/app/task-manager/finished/finished.component.ts
+++ src/app/task-manager/finished/finished.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../../Task';
+import { TasksService } from 'src/app/tasks.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-finished',
@@ -8,12 +10,12 @@ import { Task } from '../../Task';
 })
 export class FinishedComponent implements OnInit {
 
-  tasks: Array<Task>[];
+  tasks$: Observable<Array<Task>>;
 
-  constructor() { }
+  constructor(private taskService: TasksService) { }
 
   ngOnInit() {
-
+    this.tasks$ = this.taskService.getTasks()
   }
 
 }
--- src/app/task-manager/task-manager.component.ts
+++ src/app/task-manager/task-manager.component.ts
@@ -11,7 +11,7 @@ import { Subscription } from 'rxjs';
 export class TaskManagerComponent implements OnInit, OnDestroy {
 
   task$: Subscription;
-  // id$: Subscription;
+  id$: Subscription;
 
   constructor(private taskService: TasksService) { }
 
@@ -22,16 +22,16 @@ export class TaskManagerComponent implements OnInit, OnDestroy {
     if (this.task$ !== undefined) {
       this.task$.unsubscribe();
     }
-    // if(this.id$ !== undefined){
-    //   this.id$.unsubscribe();
-    // }
+    if (this.id$ !== undefined) {
+      this.id$.unsubscribe();
+    }
   }
 
   addTaskToServer(task: Task) {
     this.task$ = this.taskService.postTask(task).subscribe();
   }
 
-  // sendIdToServer(id: number){
-  //   this.id$ = this.taskService.putTask(id).subscribe();
-  // }
+  sendIdToServer(task: Task) {
+    this.id$ = this.taskService.putTask(task).subscribe();
+  }
 }
